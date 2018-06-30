5bd38a35abf07852f298cdfab59ceed696466396
Adam French <Afrench53198@gmail.com>
2018-06-29 17:52:32 -0700

    Updated dependencies and added readme

--- database/migrations/2018_06_29_203442_create_farms_table.php
+++ database/migrations/2018_06_29_203442_create_farms_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateFarmsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,21 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('farms', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->boolean('isFarmOwner');
-            $table->boolean('isRestaurantOwner');
+            $table->longText('description');
+            $table->string('street');
+            $table->string('city', 50);
+            $table->string('state', 2);
+            $table->string('zip', 12);
+            $table->string('phone', 30)->nullable();
             $table->float('latitude', 10, 6);
             $table->float('longitude', 10, 6);
-            $table->rememberToken();
+            $table->unsignedInteger('user_id');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -34,6 +38,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('farms');
     }
 }
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Introduction
+
+Farm to table is an app that is for local farmers trying to find restaurants, grocery stores, and other customers in the area.
+Through the process of farmers and restaurants signing up for the service and putting their business on display, customers will be able to browse local food sources near their location
+
+
+# Next Steps 
+
+* Create a user, farmer, and supplier sign up form.
+* Make the feeds of users show local farmers and suppliers.
+* Make the feeds of farmers show suppliers to sell to. 
+* Convert a farmer and supplier's address to lat and long upon entry using [Google's geocoding API](https://developers.google.com/maps/documentation/geocoding/intro)
+    * This is so the app can perform a lookup of distance between lat/long of the user and lat/long of farmers and suppliers
+* Allow suppliers to submit buy requests to farmers
+* Allow farmers to receive and manage buy requests through the site 
+
+
+
